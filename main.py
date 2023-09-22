8b6f5b7858b32e399a9073e1e33a24e4f1afd952
Rafael Leonardo <rafaelleonardodasilva.dv@gmail.com>
2023-09-22 14:44:50 -0300

    Feature para não filtrar o preço quando o máximo for igual a 0

--- main.py
+++ main.py
@@ -15,15 +15,23 @@ navegador = webdriver.Chrome(options=options, service=servico)
 navegador.get("https://pt.aliexpress.com/")
 
 item = input()
-preco_min = input()
-preco_max = input()
+preco_min = int(input())
+preco_max = int(input())
 navegador.find_element(by=By.ID, value="search-key").send_keys(item, Keys.ENTER)
-navegador.find_element(By.XPATH, '//*[@id="root"]/div[1]/div/div[2]/div/div[2]/div[1]/div/div[1]/div[1]/div[1]/input[1]').send_keys(preco_min)
-navegador.find_element(By.XPATH, '//*[@id="root"]/div[1]/div/div[2]/div/div[2]/div[1]/div/div[1]/div[1]/div[1]/input[2]').send_keys(preco_max)
-navegador.find_element(By.CLASS_NAME, 'priceInput--ok--2apR64x').click()
 
-while len(navegador.find_elements(By.CLASS_NAME, 'verhicles-new-content')) < 1:
-    time.sleep(1)
+if preco_max == 0:
+    pass
+else:
+    navegador.find_element(By.XPATH,
+                           '//*[@id="root"]/div[1]/div/div[2]/div/div[2]/div[1]/div/div[1]/div[1]/div[1]/input[1]').send_keys(
+        preco_min)
+    navegador.find_element(By.XPATH,
+                           '//*[@id="root"]/div[1]/div/div[2]/div/div[2]/div[1]/div/div[1]/div[1]/div[1]/input[2]').send_keys(
+        preco_max)
+    navegador.find_element(By.CLASS_NAME, 'priceInput--ok--2apR64x').click()
+
+    while len(navegador.find_elements(By.CLASS_NAME, 'priceInput--ok--2apR64x')) >= 1:
+        time.sleep(1)
 
 lista_elementos = navegador.find_elements(By.CLASS_NAME, 'manhattan--container--1lP57Ag')
 produtos = []
@@ -31,8 +39,7 @@ for elemento in lista_elementos:
     nome = elemento.find_element(By.CLASS_NAME, 'cards--title--2rMisuY').text
     preco = elemento.find_element(By.CLASS_NAME, 'manhattan--price-sale--1CCSZfK').text
     try:
-        elemento_vendas = elemento.find_element(By.CLASS_NAME, 'manhattan--tradeContainer--33O19sx')
-        n_vendas = elemento_vendas.find_element(By.CLASS_NAME, 'manhattan--trade--2PeJIEB').text
+        n_vendas = elemento.find_element(By.CLASS_NAME, 'manhattan--trade--2PeJIEB').text
     except:
         n_vendas = '0 vendas'
     link = elemento.get_attribute('href')
